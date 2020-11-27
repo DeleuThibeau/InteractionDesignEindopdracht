06240c4f06d4beaad05d9767bbb79b07942f8af1
Thibeau Deleu <thibeau.deleu@student.howest.be>
2020-11-27 19:15:23 +0100

    FILTER WERKT EINDELIJK

--- Script/Mainpage.js
+++ Script/Mainpage.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    showType();
-});
 
-let filterType =""
+// #region ==== Global Variables //
 
-//#region ===== API & pokemon HTML //
-
+let filterType ="showAll";
+let html_PokemonList;
 let nextcall ="https://pokeapi.co/api/v2/pokemon?limit=10"
-const pokemonList = document.querySelector('.js-pokemonlist');
+let nextcallFilter = "https://pokeapi.co/api/v2/pokemon?limit=10"
+
+// #endregion
+
+// #region ===== API & pokemon HTML //
 
 async function getAllPokemon_API(url) {
     // Eerst bouwen we onze url op
@@ -16,31 +17,127 @@ async function getAllPokemon_API(url) {
     const data = await fetch(url) //next call variabele
             .then(r => r.json())
             .catch((err) => console.error('an error occurred', err))
+
     let resultLijst = data.results
     let next = data.next
-    //console.log("the huidige url in de API function is: \n" + nextcall)
-    nextcall = next
-    //console.log(resultLijst)
-    //console.log("the next url in de API function is: \n" + nextcall)
 
-    resultLijst.forEach(element =>setTimeout(() => { getDetailsPokemon_API(element.url) }, 500))
- 
+    nextcall = next
+    console.log(resultLijst);
+    //console.log("filter type in de volgende call is " + filterType)
+
+    if(filterType == "showAll")
+    {
+        console.log("ALL")
+        resultLijst.forEach(element =>setTimeout(() => { getDetailsPokemon_API(element.url) }, 500))
+    }
+
+    else
+    {
+        console.log("FILTER")
+        resultLijst.forEach(element =>setTimeout(() => { getDetailsFilteredPokemon_API(element.url) }, 500))
+    }
 };
 
-const getDetailsPokemon_API = async (url) => {
+    const getDetailsPokemon_API = async (url) => {
+        await setTimeout(() => { 
+            fetch(url)
+            .then(result => {
+                result.json().then(object => { 
+                //console.log(object.types[0].type.name)
+                //console.log(object.types[1].type.name)
+                let powerlevel = ((object.stats[0].base_stat + object.stats[1].base_stat + object.stats[2].base_stat +object.stats[3].base_stat + object.stats[4].base_stat+ object.stats[5].base_stat)/1125)*100
+                
+                html_PokemonList.innerHTML += createPokemonHtml(object,powerlevel)
+                
+                if(object.id == 893)
+                {
+                    var element = document.getElementById("id_loader");
+                    element.classList.add("o-hide")
+                }
+            });
+        });
+    },500)
+};
+
+// #endregion
+
+// #region ===== Filter  //
+
+const getDetailsFilteredPokemon_API = async (url) => {
 
     await setTimeout(() => { 
-     fetch(url)
+    fetch(url)
         .then(result => {
         let data = result.json().then(object => { 
+         //console.log(object)
+        // console.log(object.types[0].type.name)
+        // console.log(object.types[1].type.name)
+        let powerlevel = ((object.stats[0].base_stat + object.stats[1].base_stat + object.stats[2].base_stat +object.stats[3].base_stat + object.stats[4].base_stat+ object.stats[5].base_stat)/1125)*100;
         //console.log(object.types)
-        //console.log(object.types[0].type.name)
-        //console.log(object.types[1].type.name)
-        let powerlevel = ((object.stats[0].base_stat + object.stats[1].base_stat + object.stats[2].base_stat +object.stats[3].base_stat + object.stats[4].base_stat+ object.stats[5].base_stat)/1125)*100
+        //console.log(element)
         
-        pokemonList.innerHTML += `
-        <div class="c-app-pokemon">
+        //console.log(`${filterType} - ${object.name}`);
+        //console.log(object);
+
+        let listFilteredPokemon = "";
+        if(filterType == object.types[0].type.name){ 
+            listFilteredPokemon += createPokemonHtml(object, powerlevel);
+            //console.log(`${filterType} - ${object.name} SUCCEEDED`);
+        }
+        //console.log(listFilteredPokemon);
+        html_PokemonList.innerHTML += listFilteredPokemon;
+    });
+        //console.log(data)
+    
+    });
+    }, 100)
+
+};
+
+
+const ShowFilterType = (element) => {
+
+    document.getElementById(element).addEventListener("click", async function(){
+        //console.log(`Er werd op ${element} geklikt`)
+        filterType = element;
+        console.log(filterType)
+        html_PokemonList.innerHTML =""
+
+        nextcall ="https://pokeapi.co/api/v2/pokemon?limit=50"
         
+    })
+};
+
+
+const FilterSelector = function(){
+    const div = document.querySelector(".overlay-content");
+    //console.log(div)
+    var list = Array.prototype.slice.call(div.children)
+    //console.log(list)
+    var typeList = []
+
+    for(i = 0 ; i < 32; i++)
+    {
+        if(i%2 == 1){
+            //console.log(list[i].id);
+            typeList.push(list[i].id)
+        }
+    }
+
+    //console.log(typeList)
+    //list.forEach(element => console.log(element.id))
+
+    typeList.forEach(ShowFilterType);
+};
+
+// #endregion
+
+// #region ==== HTML creation //
+
+const createPokemonHtml = function(object, powerlevel){
+    const htmlObj = `
+    <div class="c-app-pokemon">
+    
         <div class="c-pokemon">
             <img class="c-sprite-pokemon" src="${object.sprites.front_default}" alt="${object.name}">
             <div class="c-pokemon-name">${object.name}</div>
@@ -102,18 +199,13 @@ const getDetailsPokemon_API = async (url) => {
             </div>
         </div>
     </div>`;
+    return htmlObj;
+};
 
-        if(object.id == 893){
-            var element = document.getElementById("id_loader");
-            element.classList.add("o-hide")
-        }
-    });
-});
-}, 500)};
 
-//#endregion
+// #endregion
 
-//#region ===== Autoscroll  //
+// #region ===== Autoscroll  //
 const animateIn = document.querySelectorAll(".js-animate-in-reset");
 
 observerR = new IntersectionObserver((entries) => {
@@ -122,6 +214,7 @@ observerR = new IntersectionObserver((entries) => {
                 
                 //console.log("Visible", entry)
                 //console.log("De url die wordt gebruikt bij de IntersectionObserver is : \n" + nextcall)
+                //console.log(nextcall)
                 getAllPokemon_API(nextcall);
 
         } 
@@ -132,9 +225,9 @@ animateIn.forEach(x => {
     observerR.observe(x)
 })
 
-//#endregion
+// #endregion
 
-//#region ===== Sidebar  //
+// #region ===== Sidebar  //
 
 document.getElementById("hamburger-icon").addEventListener("click",function(){
     document.getElementById("myNav").style.width = "30%";
@@ -144,161 +237,15 @@ document.getElementById("id_closebtn").addEventListener("click",function(){
     document.getElementById("myNav").style.width = "0%";
 });
 
-//#endregion
+// #endregion
 
-//#region ===== Filter  //
+// #region ==== DOM //
+document.addEventListener('DOMContentLoaded', function() {
+    html_PokemonList = document.querySelector('.js-pokemonlist');
+    FilterSelector();
 
-
-const showType = () => {
-
-
-
-
-    document.getElementById("water").addEventListener("click", async function(){
-        console.log(`Er werd op water geklikt`)
-
-        let nextcall ="https://pokeapi.co/api/v2/pokemon?limit=10"
-        const pokemonList = document.querySelector('.js-pokemonlist');
-        filterType = "water"
-        // Eerst bouwen we onze url op
-        url = nextcall;
-        // Met de fetch API proberen we de data op te halen.		
-        const data = await fetch(url) //next call variabele
-                .then(r => r.json())
-                .catch((err) => console.error('an error occurred', err))
-        
-        let resultLijst = data.results
-        let next = data.next
-
-        //console.log("the huidige url in de API function is: \n" + nextcall)
-        nextcall = next
-        //console.log("the next url in de API function is: \n" + nextcall)
-
-        resultLijst.forEach(element =>setTimeout(() => { getDetailsPokemon_API(element.url) }, 100))
-        
-
-
-        const getDetailsPokemon_API = async (url) => {
-
-            await setTimeout(() => { 
-            fetch(url)
-                .then(result => {
-                let data = result.json().then(object => { 
-                 //console.log(object)
-                // console.log(object.types[0].type.name)
-                // console.log(object.types[1].type.name)
-                let powerlevel = ((object.stats[0].base_stat + object.stats[1].base_stat + object.stats[2].base_stat +object.stats[3].base_stat + object.stats[4].base_stat+ object.stats[5].base_stat)/1125)*100;
-                //console.log(object.types)
-                //console.log(element)
-                
-                console.log(filterType)
-                if(filterType == object.types[0].type.name){
-                    pokemonList.innerHTML = `
-                    <div class="c-app-pokemon">
-                    
-                    <div class="c-pokemon">
-                        <img class="c-sprite-pokemon" src="${object.sprites.front_default}" alt="${object.name}">
-                        <div class="c-pokemon-name">${object.name}</div>
-                    </div>
-                        
-                    <div class="c-stats-1">
-            
-                        <div class="c-stat-bar-margin-bottom">
-                            <p>Attack: <b>${object.stats[1].base_stat}</b></p>
-                            <div class="progress_bar_color bar_radius">
-                                <div class="progress_bar_percentage_color bar_radius" style="width:calc(${object.stats[1].base_stat}%/255*100)"></div>
-                            </div>
-                        </div>
-            
-                        <div class="c-stat-bar-margin-bottom">
-                            <p>Defense: <b>${object.stats[2].base_stat }</b></p>
-                            <div class="progress_bar_color bar_radius">
-                                <div class="progress_bar_percentage_color bar_radius" style="width:calc(${object.stats[2].base_stat}%/255*100)"></div>
-                            </div>
-                        </div>
-            
-                        <div class="c-stat-bar-margin-bottom">
-                            <p>HP: <b>${object.stats[0].base_stat }</b></p>
-                            <div class="progress_bar_color bar_radius">
-                                <div class="progress_bar_percentage_color bar_radius" style="width:calc(${object.stats[0].base_stat}%/255*100)"></div>
-                            </div>
-                        </div>
-                    </div>
-            
-                    <div class="c-stats-2">
-            
-                        <div class="c-stat-bar-margin-bottom">
-                            <p>Speciale Attack: <b>${object.stats[3].base_stat }</b></p>
-                            <div class="progress_bar_color bar_radius">
-                                <div class="progress_bar_percentage_color bar_radius" style="width:calc(${object.stats[3].base_stat}%/255*100)"></div>
-                            </div>
-                        </div>
-            
-                        <div class="c-stat-bar-margin-bottom">
-                            <p>Speciale Defense: <b>${object.stats[4].base_stat }</b></p>
-                            <div class="progress_bar_color bar_radius">
-                                <div class="progress_bar_percentage_color bar_radius" style="width:calc(${object.stats[4].base_stat}%/255*100)"></div>
-                            </div>
-                        </div>
-            
-                        <div class="c-stat-bar-margin-bottom">
-                            <p>Speed: <b>${object.stats[5].base_stat }</b></p>
-                            <div class="progress_bar_color bar_radius">
-                                <div class="progress_bar_percentage_color bar_radius" style="width:calc(${object.stats[5].base_stat}%/255*100)"></div>
-                            </div>
-                        </div>
-                    </div>
-            
-            
-                    <div class="c-powerlevel"> 
-                        <div class="c-powerlevel-text">Power level: <b>${ powerlevel.toFixed(2) }%</b></div>
-                        <div class=" progress_bar_powerlevel bar_radius_powerlevel">
-                            <div class="progress_bar_percentage_color_powerlevel bar_radius_powerlevel" style="width:calc(${powerlevel.toFixed(2)}%"></div>
-                        </div>
-                    </div>
-                </div>`;
-            }
-                
-            });
-                //console.log(data)
-            
-            });
-            }, 100)
-
-
-        };
-
-    })
-};
-
-// const filter_type = function(){
-//     const div = document.querySelector(".overlay-content");
-//     //console.log(div)
-//     var list = Array.prototype.slice.call(div.children)
-//     //console.log(list)
-//     var typeList = []
-
-//     for(i = 0 ; i < 32; i++)
-//     {
-//         if(i%2 == 1){
-//             //console.log(list[i].id);
-//             typeList.push(list[i].id)
-//         }
-//     }
-
-//     //console.log(typeList)
-//     //list.forEach(element => console.log(element.id))
-
-//     typeList.forEach(showType);
-// };
-
-//#endregion
-
-
-
-
-
-
+});
+// #endregion
 
 
 
